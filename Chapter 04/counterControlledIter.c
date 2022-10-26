fa6eeac27dcdbf3d248e289a4d57f263b4500254
Noel Johnson <noel.johnson6213@gmail.com>
2022-10-26 07:13:15 +0530

    Started Chapter 4

---
+++ Chapter 04/counterControlledIter.c
@@ -0,0 +1,20 @@
+//GTG
+
+// the following example shows counter controlled iterations using while loops
+
+#include <stdio.h>
+
+int main(void){
+  
+  int counter = 0;
+
+  while (counter < 5){
+    printf("%d ",  counter);
+    counter++;
+  }
+
+  puts("");
+
+}
+
+//TYJC
